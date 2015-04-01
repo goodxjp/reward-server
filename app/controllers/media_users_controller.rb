7e47a20e82052ffd5eef10bee9fa20808747fa53
kyuuki <kyuuki.japan@gmail.com>
2015-04-02 03:46:06 +0900

    ユーザー詳細画面追加

--- app/controllers/media_users_controller.rb
+++ app/controllers/media_users_controller.rb
@@ -1,9 +1,13 @@
-# -*- coding: cp932 -*-
+# -*- coding: utf-8 -*-
 class MediaUsersController < ApplicationController
+  before_action :set_media_user, only: [:show, :edit, :update, :destroy]
   skip_before_filter :verify_authenticity_token, :only => [ :create ]
 
   def index
     @media_users = MediaUser.all
+  end
+
+  def show
   end
 
   def create
@@ -16,9 +20,8 @@ class MediaUsersController < ApplicationController
   end
 
   def destroy
-    @media_user = MediaUser.find(params[:id])
     @media_user.destroy
-    head :no_content
+    redirect_to media_users_url, notice: 'MediaUser was successfully destroyed.'
   end
 
   def notify
@@ -26,13 +29,17 @@ class MediaUsersController < ApplicationController
     api_key = ENV['API_KEY']
     registration_ids = [ @media_user.android_registration_id ]
     gcm = GCM.new(api_key)
-    options = { data: { message: "�ʒm�̃e�X�g" }, collapse_key: "test" }
+    options = { data: { message: "通知のテスト" }, collapse_key: "test" }
     response = gcm.send_notification(registration_ids, options)
 
     head :no_content
   end
 
   private
+    def set_media_user
+      @media_user = MediaUser.find(params[:id])
+    end
+
     def media_user_params
       params.require(:media_user).permit(:terminal_id, :terminal_info, :android_registration_id)
     end
