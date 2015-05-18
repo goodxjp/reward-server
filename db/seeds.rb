07d9b28881fd8452395fe8f85d99b00bd2c3f664
kyuuki <kyuuki.japan@gmail.com>
2015-05-18 18:36:42 +0900

    誰でもユーザー登録できる機能を削除し、初期データを登録

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +6,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+AdminUser.create(email: 'kyuuki0@gmail.com', password: '12threer')
+
+Medium.create(name: 'リワードアプリ（仮）', key: '6Fk810vbM3')
+
+Network.create(name: '自社')
+
