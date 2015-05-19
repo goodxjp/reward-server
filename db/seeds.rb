e2b70206ed291fc3762ee5a4eee1fd22b08c289c
kyuuki <kyuuki.japan@gmail.com>
2015-05-19 10:27:49 +0900

    カテゴリのデフォルト値を追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,14 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 AdminUser.create(email: 'kyuuki0@gmail.com', password: '12threer')
+AdminUser.create(email: 'goodx.jp@gmail.com', password: 'goodx123')
+
+CampaignCategory.create(id: 1, name: 'アプリ DL')
+CampaignCategory.create(id: 2, name: '会員登録')
+CampaignCategory.create(id: 3, name: '申し込み')
+CampaignCategory.create(id: 4, name: '買い物')
 
 Medium.create(name: 'リワードアプリ（仮）', key: '6Fk810vbM3')
 
-Network.create(name: '自社')
+Network.create(id: 1, name: '自社')
 
