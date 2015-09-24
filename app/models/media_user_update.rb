b68f99e78a08a1b2e7d17f31eb9db03f73fb1733
kyuuki <kyuuki.japan@gmail.com>
2015-09-24 21:55:27 +0900

    MediaUserUpdate モデルのテストを追加

--- app/models/media_user_update.rb
+++ app/models/media_user_update.rb
@@ -1,3 +1,14 @@
+# -*- coding: utf-8 -*-
 class MediaUserUpdate < ActiveRecord::Base
   belongs_to :media_user
+
+  #
+  # Validator
+  #
+  # これって、モデルにかけるべきか？ID にかけるべきか？
+  # http://railsguides.net/belongs-to-and-presence-validation-rule1/
+  validates :media_user,
+    presence: true
+  #validates :media_user_id,
+  #  presence: true
 end
--- spec/models/media_user_update_spec.rb
+++ spec/models/media_user_update_spec.rb
@@ -1,5 +1,12 @@
+# -*- coding: utf-8 -*-
 require 'rails_helper'
 
 RSpec.describe MediaUserUpdate, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "media_user が必須" do
+    media_user_update = build(:media_user_update, media_user: nil)
+    media_user_update.valid?
+    expect(media_user_update.errors[:media_user]).to include(I18n.t('errors.messages.empty'))
+    # モデルに Validator をかけているので、外部キーにエラーは入らない。
+    expect(media_user_update.errors[:media_user_id]).to eq []
+  end
 end
