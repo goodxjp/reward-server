4df69d4444c014a671bfcadacc162c97feea5655
kyuuki <kyuuki.japan@gmail.com>
2015-09-24 22:02:51 +0900

    TerminalAndroid モデルのテストを追加

--- app/models/terminal_android.rb
+++ app/models/terminal_android.rb
@@ -1,9 +1,19 @@
+# -*- coding: utf-8 -*-
+#
+# Android 端末
+#
+# - 実際には同じ端末でも、メディアが異なれば別の端末として保持。
+#
 class TerminalAndroid < ActiveRecord::Base
   belongs_to :media_user
 
   #
   # Validation
   #
+  validates :media_user,
+    presence: true
   validates :identifier,
+    presence: true
+  validates :available,
     presence: true
 end
--- spec/models/terminal_android_spec.rb
+++ spec/models/terminal_android_spec.rb
@@ -1,5 +1,22 @@
+# -*- coding: utf-8 -*-
 require 'rails_helper'
 
 RSpec.describe TerminalAndroid, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "media_user が必須" do
+    terminal_android = build(:terminal_android, media_user: nil)
+    terminal_android.valid?
+    expect(terminal_android.errors[:media_user]).to include(I18n.t('errors.messages.empty'))
+  end
+
+  it "identifier が必須" do
+    terminal_android = build(:terminal_android, identifier: nil)
+    terminal_android.valid?
+    expect(terminal_android.errors[:identifier]).to include(I18n.t('errors.messages.empty'))
+  end
+
+  it "available が必須" do
+    terminal_android = build(:terminal_android, available: nil)
+    terminal_android.valid?
+    expect(terminal_android.errors[:available]).to include(I18n.t('errors.messages.empty'))
+  end
 end
