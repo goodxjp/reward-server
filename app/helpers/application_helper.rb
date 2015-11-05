5fb3dfe3c9d98751c80e93bc661ed4998cd4cdb1
kyuuki <kyuuki.japan@gmail.com>
2015-11-05 15:39:55 +0900

    環境によって管理画面のタイトルと色を変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,58 @@
+# -*- coding: utf-8 -*-
 module ApplicationHelper
+  def system_title
+    if production?
+      "リワードシステム"
+    elsif staging?
+      "(S) リワードシステム"
+    else
+      "(開) リワードシステム"
+    end
+  end
+
+  def system_name
+    if production?
+      "リワードシステム"
+    elsif staging?
+      "リワードシステム (S)"
+    else
+      "リワードシステム (開)"
+    end
+  end
+
+  def skin_color
+    # https://www.almsaeedstudio.com/themes/AdminLTE/documentation/index.html#layout
+    if production?
+      "skin-purple"
+    elsif staging?
+      "skin-blue"
+    else
+      "skin-black"
+      "skin-purple"
+    end
+  end
+
+  def production?
+    if (ENV['NEW_RELIC_APP_NAME'] == 'reward-production')
+      true
+    else
+      false
+    end
+  end
+
+  def staging?
+    if (ENV['NEW_RELIC_APP_NAME'] == 'reward-staging')
+      true
+    else
+      false
+    end
+  end
+
+  def development?
+    if (ENV['RACK_ENV'] == 'development')
+      true
+    else
+      false
+    end
+  end
 end
